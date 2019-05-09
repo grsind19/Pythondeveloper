4f90e58ab209ae9e18f537b642ea863c257eb58c
Seetharaman GR <grsind19@gmail.com>
2019-05-09 09:42:03 +0530

    Basics of objects oriented design

---
+++ 3. Object oriented design/1. BasicsObjects.py
@@ -0,0 +1,11 @@
+# How to define the object
+# Objects = Nouns
+
+# Things
+# People
+# Places
+# Ideas
+# Concepts
+
+# The in front of it
+# It can be object
---
+++ 3. Object oriented design/2. Class.py
@@ -0,0 +1,23 @@
+# Code template for objects
+# Coolie cutter
+# Theee components
+
+# 1. Name
+# 2. Attributes
+# 3. Behaviour
+
+# Methods as a part of 
+# Analysis, Design, Build
+# What do you need to do
+# How you are going to do
+
+# 5 step approac
+
+# 1. Gather Requirement
+# 2. Describe the application
+# 3. Identify the main objects
+# 4. Describe the interactions
+# 5. Create a class diagram
+
+# Unified modeling language
+# Wiki - List of Uniofied mode - martin fowler
